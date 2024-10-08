0200464d562d71e5a4f460e9248523ed97f25e19
Edouard26 <edouard.halimi@gmail.com>
2024-10-08 17:19:34 +0200

    Delete snippet list and snippet detail and keep viewsets & router Install React front end

--- tutorial/machine_learning/snippets/views.py
+++ tutorial/machine_learning/snippets/views.py
@@ -9,6 +9,8 @@ from rest_framework.response import Response
 from rest_framework import renderers
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
@@ -38,4 +40,10 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
