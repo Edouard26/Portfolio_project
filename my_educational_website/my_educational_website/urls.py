ae1b0eb9b986ccc069f73774b1ce32a4e50af746
Edouard26 <edouard.halimi@gmail.com>
2024-10-15 17:32:08 +0200

    build api with user class model and CRUD operations

---
+++ my_educational_website/my_educational_website/urls.py
@@ -0,0 +1,13 @@
+"""
+URLconf
+
+    Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+]
