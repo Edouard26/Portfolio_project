820f2fe6b1d4b45414350fafdfc24efbeda80efe
Edouard26 <edouard.halimi@gmail.com>
2024-10-16 09:43:51 +0200

    ~ Rename field name on user to pseudo     + Add field email to user     + Add field firstname to user     + Add field lastname to user

--- my_educational_website/api/models.py
+++ my_educational_website/api/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    pseudo = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100, default='Doe')
+    firstname = models.CharField(max_length=100, default='John')
     age = models.IntegerField()
-    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, default='user@example.com')
     
 
     def __str__(self):
-        return self.username
+        return self.pseudo
